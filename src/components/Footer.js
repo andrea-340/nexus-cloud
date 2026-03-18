ed9960cfe8314a74e5ee5eb73f79de885b9da96a
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-03-18 11:43:00 +0100

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-10 border-t border-slate-100 bg-white text-center">
-      <p className="text-xs font-black uppercase tracking-widest text-slate-400">
-        © 2026 NEXUS CLOUD • All Rights Reserved
-      </p>
-    </footer>
-  );
-}
